b5f341dd31922c9c6adf3598100f3f2e45b1fd7c
Mario <mamerida2013@gmail.com>
2021-12-29 12:55:52 -0300

    useState con Context y provider

--- src/App.js
+++ src/App.js
@@ -1,51 +1,51 @@
-import {createContext, useContext} from 'react';
+import {createContext,useContext,useState} from 'react'
 
-//para poder utilizar Context tengo que usar createContext que reciba algun dato cualquiera sea
-//este valor va a ser el mostrado en caso de no crear un provider
-//el valor por defecto se usa cuando el componente NO TIENE UN PROVIDER
-const ContextDefault = createContext('valor por defecto')
-const Context2 = createContext('valor por defecto 2 ')
 
-// provider va a ser el encargado de pasar el contexto a los componentes hijos
-//este provider se saca con la etiqueta <Context.Provider> puedo sacarlo aparte pero asi es mas especifico y puedo tener varias instancias 
-// para que context funcione tenemos que pasar un valor el cual se va a pasar a los componentes hijos 
-const DefaultProvider = ({children}) =>{
-  return(
-    <ContextDefault.Provider value={"mi valor "}>
-      {children}
-    </ContextDefault.Provider>
-  )
-}
-//componente creado para ver cuando se utiliza el valor por defecto  
-const Contenido2 = () =>{
-  //para poder tener acceso al contexto le paso el componente  como parametro en useContext 
-  //en este caso como yo no cree un contextProvider con Context2 no voy a tener un mensaje por ende usa el mensaje por defecto 
-  const ctx = useContext(Context2)
-  return(
-    <div>
-      {ctx}
-    </div>
-  )
-}
-const Contenido = () =>{
-  //para poder tener acceso al contexto le paso el componente  como parametro en useContext 
-  const ctx = useContext(ContextDefault)
-  return(
-    <div>
-      {ctx}
-    </div>
-  )
+const Context = createContext({valor:false, toggle: () =>{ console.log("test ")}})
+
+const Provider = ({children}) =>{
+    const[valor,setValor] = useState(false)
+    const value = {
+        valor, 
+        toggle: ()=> setValor(!valor)
+    }
+    return(
+        <Context.Provider value={value}>
+            {children}
+        </Context.Provider>
+    )
 }
 
-
-
-function App() {
-  return (
-    <DefaultProvider>
-      <Contenido/>
-      <Contenido2/>
-    </DefaultProvider>
-  );
+const Componente = () =>{
+    //para poder acceder al contexto tengo que acceder a useContext con el objeto que le va a dar el contecto de las propiedades 
+    const { valor , toggle} = useContext(Context)
+    return(
+        <div>
+            <label>{valor.toString()}</label>
+            <button onClick={toggle}>Toggle</button>
+        </div>
+    )
+}
+const Componente2 = () =>{
+    //para poder acceder al contexto tengo que acceder a useContext con el objeto que le va a dar el contecto de las propiedades 
+    const { valor , toggle} = useContext(Context)
+    return(
+        <div>
+            <label>{valor.toString()}</label>
+            <button onClick={toggle}>Toggle</button>
+        </div>
+    )
 }
 
-export default App;
+const App  = () =>{
+    return(
+        <div>
+        <Provider>
+            <Componente/>
+        </Provider>
+        <Componente2/>
+        </div>
+    )
+}
+
+export default App
